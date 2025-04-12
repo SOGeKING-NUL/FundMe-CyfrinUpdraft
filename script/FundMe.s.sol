74cbc97fa0f81a7951b3dbdec47e6becded253d6
sogeking-nul <utsav.jana.5@gmail.com>
2025-04-12 16:14:43 +0530

    priceConverter.sol done

--- script/FundMe.s.sol
+++ script/FundMe.s.sol
@@ -1,19 +1,18 @@
-// SPDX-Licence-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
 import {Script} from "lib/forge-std/src/Script.sol";
 import {console} from "liB/forge-std/src/console.sol";
 import {FundMe} from "src/FundMe.sol";
 
 contract FundMeScript is Script{
-    FundMe public fundme;
-    
+   
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        fundme = new FundMe();
+        FundMe fundme = new FundMe();
         console.log(fundme.Price());
 
         vm.stopBroadcast();
--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,84 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+
+/*
+-> import aggregator, price convertor
+-> contructor has owner
+-> fund checks for enough money to fund ad adds to contract
+-> get aggregator version
+-> withdraw funds
+-> fallback and revert
+*/
+
 import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error NotOwner();
+contract FundMe{
 
-contract FundMe {
     using PriceConverter for uint256;
+    address public i_owner;
+    uint256 price;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
-
-    address public immutable i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-
-    constructor() {
+    constructor(){
         i_owner = msg.sender;
     }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        // call
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
-
-    fallback() external payable {
-        fund();
-    }
-
-    receive() external payable {
-        fund();
-    }
+    function fund(uint256 ethAmount) public payble
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
